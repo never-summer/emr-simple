783f166c9be1bc5c1719fe19f347bd6772af84af
max <max@max-desktop>
2016-01-15 10:43:51 +0600

    start add warc

--- src/main/java/ru/never_summer/emr_sample/Driver.java
+++ src/main/java/ru/never_summer/emr_sample/Driver.java
@@ -1,5 +1,13 @@
 package ru.never_summer.emr_sample;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.zip.GZIPInputStream;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -10,6 +18,9 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Driver {
 	public static void main(String[] args) throws Exception {
+		if (args.length < 1) {
+			System.out.println("Usage: <inDir> <outDir> and optional<countReducer>");
+		}
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf, "simple emr site count");
 		job.setJarByClass(Driver.class);
@@ -17,8 +28,37 @@ public class Driver {
 		job.setReducerClass(Reduce.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
+		// if exist, set count reducer
+		if (args.length == 2) {
+			try {
+				int reducerCount = Integer.parseInt(args[2]);
+				job.setNumReduceTasks(reducerCount);
+			} catch (Exception e) {
+				System.out.println("Can't parse count reducer");
+			}
+		}
+		// add input paths for job
 		FileInputFormat.addInputPath(job, new Path(args[0]));
+		// addInputPaths(args[0], job);
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+
+	public static void addInputPaths(Job job, String urlFeil) throws IOException {
+		final String awsPublic = "s3://aws-publicdatasets/";
+		URL url = new URL(urlFeil);
+		URLConnection connection = url.openConnection();
+		InputStream is = connection.getInputStream();
+		GZIPInputStream gzis = new GZIPInputStream(is);
+		InputStreamReader xover = new InputStreamReader(gzis);
+		BufferedReader br = new BufferedReader(xover);
+
+		String line;
+		// Now read lines of text: the BufferedReader puts them in lines,
+		// the InputStreamReader does Unicode conversion, and the
+		// GZipInputStream "gunzip"s the data from the FileInputStream.
+		while ((line = br.readLine()) != null)
+			FileInputFormat.addInputPath(job, new Path(awsPublic + line));
+
 	}
 }
--- src/main/java/ru/never_summer/emr_sample/Map.java
+++ src/main/java/ru/never_summer/emr_sample/Map.java
@@ -1,21 +1,28 @@
 package ru.never_summer.emr_sample;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class Map extends Mapper<Object, Text, Text, IntWritable> {
-
+	private static final Pattern pattern = Pattern.compile("(https?|http)://*");
 	private final static IntWritable one = new IntWritable(1);
 
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-		try {
-			if (value.toString().matches("(https)://.*"))
-				context.write(value, one);
-		} catch (Exception e) {
-			e.printStackTrace();
+		String tmp = value.toString();
+		Matcher matcher = pattern.matcher(tmp);
+		if (matcher.find()) {
+			try {
+				URL url = new URL(tmp);
+				context.write(new Text(url.getHost()), one);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
